b48bec11d44ee7587ea644e251f6e48174e4c4c3
N0N4M3ND <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-04 15:23:24 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -5,7 +5,7 @@ mount /dev/sda3 /mnt &&
 swapon /dev/sda2 &&
 mount --mkdir /dev/sda1 /mnt/boot/efi &&
 timedatectl set-ntp true &&
-pacstrap /mnt linux linux-firmware base base-devel grub efibootmgr nano vim networkmanager xorg lightdm-webkit2-greeter bspwm sxhkd alacritty ttf-liberation rofi pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol git neofetch lxappearance nitrogen picom polybar unzip amd-ucode nvidia && pacstrap /mnt mpv obs-studio firefox &&
+pacstrap /mnt linux linux-firmware base base-devel grub efibootmgr nano vim networkmanager xorg lightdm-webkit2-greeter bspwm sxhkd alacritty ttf-liberation noto-fonts-emoji rofi pipewire pipewire-alsa pipewire-pulse pipewire-jack pavucontrol git neofetch lxappearance nitrogen picom polybar unzip amd-ucode nvidia && pacstrap /mnt mpv obs-studio firefox &&
 genfstab -U /mnt > /mnt/etc/fstab &&
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/Asia/Tbilisi /etc/localtime &&
 arch-chroot /mnt sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen &&
